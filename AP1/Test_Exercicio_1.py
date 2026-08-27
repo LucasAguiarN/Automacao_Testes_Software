dcb1e3af4e0598d40b68ded77492cdf5f8a821f9
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-08-27 16:31:29 -0300

    ajuste exercicio ap1 1

--- AP1/Test_Exercicio_1.py
+++ AP1/Test_Exercicio_1.py
@@ -11,8 +11,11 @@ class TestDivisao(unittest.TestCase):
     def test_divisao_por_zero(self):
         self.assertRaises(ValueError, Exercicio_1.divisao, 10, 0)
 
-    def test_divisao_por_numero_invalido(self):
+    def test_divisao_por_numero_invalido_input_1(self):
         self.assertRaises(TypeError, Exercicio_1.divisao, "10", 2)
+
+    def test_divisao_por_numero_invalido_input_2(self):
+        self.assertRaises(TypeError, Exercicio_1.divisao, 10, "2")
 
 if __name__ == '__main__':
     unittest.main()
