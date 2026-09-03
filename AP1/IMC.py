47dfbc5870ff33a07bff50efe175f6ab5fc8ef4e
Lucas Aguiar Nunes <2403912@salas.aulas>
2026-09-02 21:33:31 -0300

    add exercicio 2 ap1

---
+++ AP1/IMC.py
@@ -0,0 +1,16 @@
+def calcular_imc(nome, peso, altura):
+    imc = peso / (altura**2)
+    if type(peso) != float and type(altura) != float:
+        raise TypeError()
+    if imc < 18.5:
+        return f'{nome} seu IMC é Abaixo do Peso'
+    elif imc >= 18.5 and imc <= 24.9:
+        return f'{nome} seu IMC é Peso Normal'
+    elif imc >= 25.0 and imc <= 29.9:
+        return f'{nome} seu IMC é Sobrepeso'
+    elif imc >= 30.0 and imc <= 34.9:
+        return f'{nome} seu IMC é Obesidade Grau I'
+    elif imc >= 35.0 and imc <= 39.9:
+        return f'{nome} seu IMC é Obesidade Grau II'
+    elif imc >= 40:
+        return f'{nome} seu IMC é Obesidade Grau III'
