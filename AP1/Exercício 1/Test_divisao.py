e8a41202ed83171ffbe0cdb4c1aef28643508924
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-09-14 12:05:36 -0300

    arrumando diretorios

--- AP1/Exercício 1/Test_divisao.py
+++ AP1/Exercício 1/Test_divisao.py
@@ -1,21 +1,21 @@
 import unittest
-import Exercicio_1
+import divisao
 
 class TestDivisao(unittest.TestCase):
     def test_divisao_por_numero_inteiro(self):
-        self.assertEqual(Exercicio_1.divisao(10, 2), 5)
+        self.assertEqual(divisao.divisao(10, 2), 5)
 
     def test_divisao_por_numero_float(self):
-        self.assertEqual(Exercicio_1.divisao(10, 2.5), 4)
+        self.assertEqual(divisao.divisao(10, 2.5), 4)
 
     def test_divisao_por_zero(self):
-        self.assertRaises(ValueError, Exercicio_1.divisao, 10, 0)
+        self.assertRaises(ValueError, divisao.divisao, 10, 0)
 
     def test_divisao_por_numero_invalido_input_1(self):
-        self.assertRaises(TypeError, Exercicio_1.divisao, "10", 2)
+        self.assertRaises(TypeError, divisao.divisao, "10", 2)
 
     def test_divisao_por_numero_invalido_input_2(self):
-        self.assertRaises(TypeError, Exercicio_1.divisao, 10, "2")
+        self.assertRaises(TypeError, divisao.divisao, 10, "2")
 
 if __name__ == '__main__':
     unittest.main()
